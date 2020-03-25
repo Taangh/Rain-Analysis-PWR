4c15f3c5950b2e17ef21c163463bb32ae3c088ed
Damian Szczygielski <dszczygielski@farutex.pl>
2020-03-25 21:18:50 +0100

    fixes in returns

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import numpy as np
 from netCDF4 import Dataset
 from collections import OrderedDict
-import numpy as np
 from enum import Enum
 from datetime import datetime
 
 
 class Cities(Enum):
-	SZCZECIN = 0
-	KOSTRZYN_NAD_ODRA = 1
-	NOWA_SOL = 2
-	TRESTNO = 3
+    SZCZECIN = 0
+    KOSTRZYN_NAD_ODRA = 1
+    NOWA_SOL = 2
+    TRESTNO = 3
 
 
 def read_netcdf(netcdf_file):
-	contents = OrderedDict()
-	data = Dataset(netcdf_file, 'r')
-	for var in data.variables:
-		attrs = data.variables[var].ncattrs()
-		contents[var] = data.variables[var][:]
-	data = contents['precip']
-	if len(data.shape) == 3:
-		data = data.swapaxes(0, 2)
-		data = data.swapaxes(0, 1)
-		return data
-	else:
-		return data
+    contents = OrderedDict()
+    data = Dataset(netcdf_file, 'r')
+    for var in data.variables:
+        contents[var] = data.variables[var][:]
+    data = contents['precip']
+    if len(data.shape) == 3:
+        data = data.swapaxes(0, 2)
+        data = data.swapaxes(0, 1)
+        return data.data
+    else:
+        return data.data
 
 
 def calculate_day_of_year(date):
@@ -35,13 +33,13 @@ def calculate_day_of_year(date):
 def get_rain_amount_array_for_city(array_wit_rain_data, city):
     idx_lat = int((start_lat - coords[city.value][0]) / 0.04)
     idx_lon = int((coords[city.value][1] - start_lon) / 0.04)
-    print("Year average for", city.name, "is:", np.sum(array_wit_rain_data[idx_lat][idx_lon])/365)
+    return np.round(np.sum(array_wit_rain_data[idx_lat][idx_lon]) / 365, 2)
 
 
 def get_rain_amount_for_city_and_date(array_wit_rain_data, city, date):
     idx_lat = int((start_lat - coords[city.value][0]) / 0.04)
     idx_lon = int((coords[city.value][1] - start_lon) / 0.04)
-    print("Amount of rain for", city.name, "is:", array_wit_rain_data[idx_lat][idx_lon][date])
+    return array_wit_rain_data[idx_lat][idx_lon][date]
 
 
 # Constants
@@ -52,11 +50,11 @@ coords = np.array([[53.44, 14.36], [52.72, 14.40], [51.48, 15.44], [51.04, 17.08
 
 
 def main():
-    get_rain_amount_array_for_city(data.data, Cities.SZCZECIN)
-    get_rain_amount_array_for_city(data.data, Cities.KOSTRZYN_NAD_ODRA)
-    get_rain_amount_array_for_city(data.data, Cities.NOWA_SOL)
-    get_rain_amount_array_for_city(data.data, Cities.TRESTNO)
-    get_rain_amount_for_city_and_date(data.data, Cities.SZCZECIN, calculate_day_of_year("1.01.2018"))
+    print("Average year amount of rain for", Cities.SZCZECIN, "is:", get_rain_amount_array_for_city(data, Cities.SZCZECIN))
+    print("Average year rain for", Cities.KOSTRZYN_NAD_ODRA, "is:", get_rain_amount_array_for_city(data, Cities.KOSTRZYN_NAD_ODRA))
+    print("Average year rain for", Cities.NOWA_SOL, "is:", get_rain_amount_array_for_city(data, Cities.NOWA_SOL))
+    print("Average year rain for", Cities.TRESTNO, "is:", get_rain_amount_array_for_city(data, Cities.TRESTNO))
+    print("Amount of rain on the 1.01.2018 for", Cities.TRESTNO, "is:", get_rain_amount_for_city_and_date(data, Cities.SZCZECIN, calculate_day_of_year("1.01.2018")))
 
 
 if __name__ == '__main__':
